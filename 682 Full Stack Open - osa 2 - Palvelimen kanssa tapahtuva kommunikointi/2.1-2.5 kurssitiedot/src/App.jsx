9d92e0b5ef3f83bb8d9aef1a86a5713a17ed387a
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-12 11:15:22 +0300

    valmis

--- 682 Full Stack Open - osa 2 - Palvelimen kanssa tapahtuva kommunikointi/2.1-2.5 kurssitiedot/src/App.jsx
+++ 682 Full Stack Open - osa 2 - Palvelimen kanssa tapahtuva kommunikointi/2.1-2.5 kurssitiedot/src/App.jsx
@@ -1,58 +1,11 @@
-/*const Total = ({course}) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p>Number of exercises {totalExercises}</p>
-}*/
-const Course = ( {course} ) => {
-  return (
-    <div>
-      <Header course={course}/>
-      <Content course={course.parts}/>
-    </div>
-  );
-};
+import Courses from "./Courses"
 
-const Header = ({course}) => {
-  return <h1>{course.name}</h1>
-};
-
-const Content = ({course}) => {
-  const total = course.reduce((sum, part) => sum + part.exercises, 0); 
-  return (
-    <div>
-      {course.map((part, index) => (
-        <Part key={index} part={part}/>
-      ))}
-      <p><strong>Total of {total} exercises</strong></p>
-    </div>
-  );
-};
-
-const Part = ({part}) => {
-  return <p>{part.name} {part.exercises}</p>
-};
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+const App = ( {courses} ) => {
 
   return (
     <div>
-      <Course course={course}/>
+      <h1>Web development curriculum</h1>
+      <Courses courses={courses}/>
     </div>
   )
 }
