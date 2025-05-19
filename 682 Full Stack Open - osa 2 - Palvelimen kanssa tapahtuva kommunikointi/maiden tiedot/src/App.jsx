cff4c5783749d3717e5989657723d7926a819b9f
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-05-19 11:24:00 +0300

    maan tiedot valmis

--- 682 Full Stack Open - osa 2 - Palvelimen kanssa tapahtuva kommunikointi/maiden tiedot/src/App.jsx
+++ 682 Full Stack Open - osa 2 - Palvelimen kanssa tapahtuva kommunikointi/maiden tiedot/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Weather = ( {capital, api_key} ) => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`)
+    .then(response => setWeather(response.data))
+    }, [capital, api_key]);
+
+    if (!weather) return <p>Loading weather data...</p>
+
+    return (
+      <div>
+        <h3>Weather in {capital}</h3>
+        <p>Temperature: {weather.main.temp}°C</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+        <p>Wind: {weather.wind.speed} m/s</p>
+      </div>
+    )
+}
+
 const App = () => {
+  const api_key = import.meta.env.VITE_SOME_KEY
   const [countries, setCountries] = useState([])
   const [searchCountry, setSearchCountry] = useState("")
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  
 
   useEffect(() => {
     axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
       .then(response => setCountries(response.data))
       .catch(error => console.log("Error fetching data:", error));
   }, []);
+
 
   const filteredCountries = searchCountry
     ? countries.filter(country =>
@@ -25,32 +50,57 @@ const App = () => {
         placeholder='Enter country name...'
         onChange={(e) => setSearchCountry(e.target.value)}
       />
-      {searchCountry && (
-        filteredCountries.length > 10 ? (
-          <p>Too many matches, specify your search</p>
-        ) : filteredCountries.length === 1 ? (
-          filteredCountries.map(country => (
-          <div key={country.cca3}>
-            <h1>{country.name.common}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area}</p>
-            <h2>Languages</h2>
+      
+      {selectedCountry ? (
+  
+        <div key={selectedCountry.cca3}>
+          <button onClick={() => setSelectedCountry(null)}>Back</button>
+          <h1>{selectedCountry.name.common}</h1>
+          <p>Capital: {selectedCountry.capital}</p>
+          <p>Area: {selectedCountry.area}</p>
+          <h2>Languages</h2>
+          <ul>
+            {selectedCountry.languages && Object.entries(selectedCountry.languages).map(([code, name]) => (
+              <li key={code}>{name}</li>
+            ))}
+          </ul>
+          <img src={selectedCountry.flags.png} />
+          <Weather capital={selectedCountry.capital} api_key={api_key}/>
+        </div>
+      ) : (
+        
+        searchCountry && (
+          filteredCountries.length > 10 ? (
+            <p>Too many matches, specify your search</p>
+          ) : filteredCountries.length === 1 ? (
+            filteredCountries.map(country => (
+              <div key={country.cca3}>
+                <h1>{country.name.common}</h1>
+                <p>Capital: {country.capital}</p>
+                <p>Area: {country.area}</p>
+                <h2>Languages</h2>
+                <ul>
+                  {country.languages && Object.entries(country.languages).map(([code, name]) => (
+                    <li key={code}>{name}</li>
+                  ))}
+                </ul>
+                <img src={country.flags.png} />
+                <Weather capital={country.capital} api_key={api_key} />
+              </div>
+            ))
+          ) : (
             <ul>
-              {country.languages && Object.entries(country.languages).map(([code, name]) => (
-                <li key={code}>{name}</li>
+              {filteredCountries.map(country => (
+                <div key={country.cca3} style={{display: "flex", gap: "10px"}}>
+                  <li>{country.name.common}</li>
+                  <button onClick={() => setSelectedCountry(country)}>Show</button>
+                </div>
               ))}
             </ul>
-            <img src={country.flags.png}/>
-          </div>
-        ))
-      ) : (
-        <ul>
-          {filteredCountries.map(country => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))}
-        </ul>
-      )
-    )}
+          )
+        )
+      )}
+      
     </div>
   )  
 }
