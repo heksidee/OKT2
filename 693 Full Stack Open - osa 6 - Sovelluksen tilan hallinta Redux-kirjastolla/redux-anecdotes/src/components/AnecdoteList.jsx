0a76c29ba58e1883aed31a706bea9d472da4e934
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-09-08 13:14:08 +0300

    teht 6.9

--- 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/components/AnecdoteList.jsx
+++ 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,11 +3,14 @@ import { vote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
-    const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes -a.votes)
+    const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state => state.filter)
+    const filteredAnecdotes = anecdotes.filter(anecdote =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+    const sortedAnecdotes = [...filteredAnecdotes].sort((a, b) => b.votes -a.votes)
 
     const voteAnecdote = (id) => {
-        //console.log('vote', id)
         dispatch(vote(id))
     }
 
