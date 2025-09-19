866d4975f624589d9c987f0357db557b4c17a32f
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-09-19 14:42:47 +0300

    backend

--- 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,17 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from "../services/anecdotes"
 import { setNotification, clearNotification } from "../reducers/notificationReducer";
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ""
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`You added: "${content}"`))
     setTimeout(() => {
       dispatch(clearNotification())
--- 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/main.jsx
+++ 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/redux-anecdotes/src/main.jsx
@@ -2,8 +2,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from "./store"
+import anecdoteService from "./services/anecdotes"
+import { setAnecdotes } from "./reducers/anecdoteReducer"
 
-console.log(store.getState())
+anecdoteService.getAll().then(anecdotes => 
+  store.dispatch(setAnecdotes(anecdotes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
