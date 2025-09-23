bfcfbc063a42c9c21554ed73e83eab34afcb18e9
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-09-23 11:27:24 +0300

    tehtävät 6.20-6.22

--- 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/query-anecdotes/src/App.jsx
+++ 693 Full Stack Open - osa 6 - Sovelluksen tilan hallinta Redux-kirjastolla/query-anecdotes/src/App.jsx
@@ -1,19 +1,36 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+  
+  const updateAnecdoteMutation = useMutation(
+    {mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] })
+    },
+  })
+
   const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: 1,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+  if (result.isLoading) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  const anecdotes = result.data
 
   return (
     <div>
