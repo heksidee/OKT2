4d2b50d8688af9adcf7a0878240fd318c362a80b
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-09-25 14:41:36 +0300

    teht 7.7

--- 694 Full Stack Open - osa 7 - React router, custom-hookit, tyylikirjastot ja webpack/country-hook/src/App.jsx
+++ 694 Full Stack Open - osa 7 - React router, custom-hookit, tyylikirjastot ja webpack/country-hook/src/App.jsx
@@ -16,9 +16,21 @@ const useField = (type) => {
 }
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState([])
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) return
+
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        setCountry({ data: response.data, found: true })
+      } catch (error) {
+        setCountry({ found: false})
+      }
+    }
+    fetchCountry()
+  }, [name])
 
   return country
 }
@@ -38,10 +50,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`}/>  
     </div>
   )
 }
