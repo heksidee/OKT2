0e70babb7e90ec1e281ed518d64986221e6084e6
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-09-26 09:57:46 +0300

    teht 7.7

--- 694 Full Stack Open - osa 7 - React router, custom-hookit, tyylikirjastot ja webpack/ultimate-hooks/src/App.jsx
+++ 694 Full Stack Open - osa 7 - React router, custom-hookit, tyylikirjastot ja webpack/ultimate-hooks/src/App.jsx
@@ -18,10 +18,16 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
