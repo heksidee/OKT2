a993002ef1bce746eb097acd1d6ece2b26e24e5f
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-10-07 15:19:31 +0300

    teht 8.1-8.3

--- 701 Full Stack Open - osa 8 - GraphQL/library-backend/index.js
+++ 701 Full Stack Open - osa 8 - GraphQL/library-backend/index.js
@@ -27,20 +27,6 @@ let authors = [
   },
 ];
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
-
 let books = [
   {
     title: "Clean Code",
@@ -99,13 +85,39 @@ let books = [
 
 const typeDefs = `
   type Query {
-    dummy: Int
+    authorCount: Int!
+    bookCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
+  
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+    }
+  
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+    }
 `;
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    authorCount: () => authors.length,
+    bookCount: () => books.length,
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (parent) => {
+      return books.filter((book) => book.author === parent.name).length;
+    },
   },
 };
 
