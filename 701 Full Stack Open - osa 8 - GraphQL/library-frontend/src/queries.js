e159ceaea9cb63fd7c8b9b3d502ce01232cf704f
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-10-09 15:53:30 +0300

    teht 8.8.-8.9

---
+++ 701 Full Stack Open - osa 8 - GraphQL/library-frontend/src/queries.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`;
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+/*export const ADD_BOOK = gql`
+    mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]) {
+        }`;*/
