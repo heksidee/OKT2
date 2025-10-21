714102d88abcb421b8ca4fa392e5f4b4f54cc76c
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-10-21 12:32:22 +0300

    teht 9.2-9.3

--- 702 Full Stack Open - osa 9 - Type Script/teht 9.1-9.3/bmiCalculator.ts
+++ 702 Full Stack Open - osa 9 - Type Script/teht 9.1-9.3/bmiCalculator.ts
@@ -1,4 +1,23 @@
-const calculateBmi = (weight: number, height: number): string => {
+interface Values {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): Values => {
+  if (process.argv.length !== 4) {
+    throw new Error("Incorrect amount of arguments");
+  }
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
 
@@ -13,4 +32,14 @@ const calculateBmi = (weight: number, height: number): string => {
   }
 };
 
-console.log(calculateBmi(100, 190));
+try {
+  const { height, weight } = parseArguments(process.argv);
+  const result = calculateBmi(height, weight);
+  console.log(result);
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened. ";
+  if (error instanceof Error) {
+    errorMessage += "Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ 702 Full Stack Open - osa 9 - Type Script/teht 9.1-9.3/exerciseCalculator.ts
@@ -0,0 +1,76 @@
+interface Hours {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: String;
+  target: number;
+  average: number;
+}
+
+const parseArgum = (
+  args: string[]
+): { target: number; dailyHours: number[] } => {
+  if (args.length < 4) {
+    throw new Error("Not enough arguments");
+  }
+
+  const target = Number(args[2]);
+  const dailyHours = args.slice(3).map((n) => Number(n));
+
+  if (isNaN(target) || dailyHours.some((n) => isNaN(n))) {
+    throw new Error("All values must be numbers");
+  }
+
+  return {
+    target,
+    dailyHours,
+  };
+};
+
+const calculateExercises = (target: number, dailyHours: number[]): Hours => {
+  const periodLength = dailyHours.length;
+  const trainingDays = dailyHours.filter((h) => h > 0).length;
+  const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength;
+
+  let success: boolean;
+  if (average >= target) {
+    success = true;
+  } else {
+    success = false;
+  }
+
+  let rating: number;
+  let ratingDescription: string;
+  if (average < target * 0.75) {
+    rating = 1;
+    ratingDescription = "You could do better";
+  } else if (average < target) {
+    rating = 2;
+    ratingDescription = "Good job!";
+  } else {
+    rating = 3;
+    ratingDescription = "Splendid!!!";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+try {
+  const { target, dailyHours } = parseArgum(process.argv);
+  console.log(calculateExercises(target, dailyHours));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong. ";
+  if (error instanceof Error) {
+    errorMessage += "Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
