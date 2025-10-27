768b6470b60a86915ec24795a72ea851b8c0cfee
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-10-27 10:45:56 +0200

    teht 9.14

--- 702 Full Stack Open - osa 9 - Type Script/teht 9.8-back/src/types.ts
+++ 702 Full Stack Open - osa 9 - Type Script/teht 9.8-back/src/types.ts
@@ -1,3 +1,6 @@
+import { z } from "zod";
+import { NewEntrySchema } from "./utils";
+
 export interface DiagnoseEntry {
   code: string;
   name: string;
@@ -8,17 +11,13 @@ export interface PatientEntry {
   id: string;
   name: string;
   dateOfBirth: string;
-  ssn: string;
+  ssn?: string;
   gender: Gender;
   occupation: string;
 }
 
-export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other",
-}
+export type Gender = "male" | "female" | "other";
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, "ssn">;
 
-export type NewPatientEntry = Omit<PatientEntry, "id">;
+export type NewPatientEntry = z.infer<typeof NewEntrySchema>;
--- 702 Full Stack Open - osa 9 - Type Script/teht 9.8-back/src/utils.ts
+++ 702 Full Stack Open - osa 9 - Type Script/teht 9.8-back/src/utils.ts
@@ -1,80 +1,14 @@
-import { Gender, NewPatientEntry } from "./types";
+import { NewPatientEntry } from "./types";
+import { z } from "zod";
 
-const isString = (text: unknown): text is string => {
-  return typeof text === "string" || text instanceof String;
+export const NewEntrySchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.string().refine((val) => !isNaN(Date.parse(val))),
+  ssn: z.string().refine((val) => /^\d{6}-[0-9A-Za-z]{4}$/.test(val)),
+  gender: z.enum(["male", "female", "other"]),
+  occupation: z.string(),
+});
+
+export const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  return NewEntrySchema.parse(object);
 };
-
-const isDate = (date: string): boolean => {
-  return !isNaN(Date.parse(date));
-};
-
-const isSSN = (ssn: string): boolean => {
-  const ssnRegex = /^\d{6}-[0-9A-Za-z]{4}$/;
-  return ssnRegex.test(ssn);
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((g) => g.toString())
-    .includes(param);
-};
-
-const parseName = (name: unknown): string => {
-  if (!isString(name)) {
-    throw new Error("Incorrect or missing name");
-  }
-  return name;
-};
-
-const parseDateOfBirth = (dateOfBirth: unknown): string => {
-  if (!isString(dateOfBirth) || !isDate(dateOfBirth)) {
-    throw new Error("Incorrect or missing date of birth: " + dateOfBirth);
-  }
-  return dateOfBirth;
-};
-
-const parseSSN = (ssn: unknown): string => {
-  if (!isString(ssn) || !isSSN(ssn)) {
-    throw new Error("Incorrect or missing SSN: " + ssn);
-  }
-  return ssn;
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!isString(gender) || !isGender(gender)) {
-    throw new Error("Incorrect or missing gender: " + gender);
-  }
-  return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!isString(occupation)) {
-    throw new Error("Incorrect or missing occupation");
-  }
-  return occupation;
-};
-
-const toNewPatientEntry = (object: unknown): NewPatientEntry => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data");
-  }
-  if (
-    "name" in object &&
-    "dateOfBirth" in object &&
-    "ssn" in object &&
-    "gender" in object &&
-    "occupation" in object
-  ) {
-    const newEntry: NewPatientEntry = {
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSSN(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation),
-    };
-    return newEntry;
-  }
-  throw new Error("Incorrect data: some fields ase missing");
-};
-
-export default toNewPatientEntry;
