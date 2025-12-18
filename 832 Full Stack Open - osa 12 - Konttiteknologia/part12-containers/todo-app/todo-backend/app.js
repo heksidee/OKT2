267ef1339f3182c9de4d7d1644167071cb60ff72
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-12-18 15:41:37 +0200

    teht 12.6-12.7

--- 832 Full Stack Open - osa 12 - Konttiteknologia/part12-containers/todo-app/todo-backend/app.js
+++ 832 Full Stack Open - osa 12 - Konttiteknologia/part12-containers/todo-app/todo-backend/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
 
 const app = express();
+
+const mongoUrl = process.env.MONGO_URL;
+console.log('Connecting to MongoDB:', mongoUrl);
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 app.use(cors());
 
--- 832 Full Stack Open - osa 12 - Konttiteknologia/part12-containers/todo-app/todo-backend/routes/todos.js
+++ 832 Full Stack Open - osa 12 - Konttiteknologia/part12-containers/todo-app/todo-backend/routes/todos.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
@@ -12,38 +12,45 @@ router.get('/', async (_, res) => {
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
-
-  next()
-}
+  const { id } = req.params;
+  try {
+    req.todo = await Todo.findById(id);
+    if (!req.todo) return res.sendStatus(404);
+    next();
+  } catch (err) {
+    return res.status(400).json({ error: 'Invalid ID format' });
+  }
+};
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.json(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.text = req.body.text ?? req.todo.text;
+  req.todo.done = req.body.done ?? req.todo.done;
+
+  await req.todo.save();
+
+  res.json(req.todo); // Implement this
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use('/:id', findByIdMiddleware, singleRouter);
 
 module.exports = router;
